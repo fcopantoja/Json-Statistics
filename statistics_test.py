20a4caf91d3d00a0451782d6b78385b37717054e
Francisco Pantoja <fcopantoja@gmail.com>
2016-02-18 01:12:20 -0600

    Update statistics_test.py

--- statistics_test.py
+++ statistics_test.py
@@ -83,3 +83,21 @@ if __name__ == '__main__':
 
     r = requests.post(url, data=processed_user_data)
     print('Response:', r.content)
+    
+    
+#### Complexity
+"""
+The complexity of iterate through all users and get emails, april_emails, unique_emails and domain_counts is O(n).
+
+
+I use a dictionary that keep track of the count of unique domains, then a dict comprehension filter domains with count greater than 1 which is an algorithm with complexity O(m) where m is less or equal than n.
+
+
+The best case scenario means all emails belong to same domain and complexity such that:
+
+O(n) + O(1) ≈ O(n)
+
+The worst case scenario means all emails domains are different and complexity such that:
+
+O(n) + O(n) = 2(O(n)) ≈ O(n)
+"""
